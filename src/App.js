e280d50f5b7d4907ff9497a918b5d219c6521b10
JaberHPranto <jhpranto53@gmail.com>
2021-12-14 22:06:14 +0600

    created basic components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Layout } from "antd";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="app">
+      <div className="navbar"></div>
+      <Navbar />
+      <div className="main"></div>
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
 export default App;
